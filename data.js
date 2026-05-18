1730d1639e655087204368796331daf429c3d516
DJ-Juang <derjengjuang@gmail.com>
2026-05-18 10:22:24 +0800

    Add rawData array with initial data entries

---
+++ data.js
@@ -0,0 +1,7 @@
+// data.js
+const rawData = [
+    {id: 1, year: "2024", region: "中國", country: "上海", url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view", memo1: "(外灘)"},
+    {id: 2, year: "2024", region: "中國", country: "上海", url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view", memo1: ""},
+    // ... 請在這裡貼上你所有的資料 ...
+    {id: 202, year: "2025", region: "西歐", country: "荷蘭", url: "https://drive.google.com/file/d/...", memo1: ""} 
+];
