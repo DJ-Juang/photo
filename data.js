3286a332c820ca3bcf12dd9b1c6daf460cf2ddc8
DJ-Juang <derjengjuang@gmail.com>
2026-06-14 12:05:26 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 // data.js
 const rawData = [
+{id: 0, year: "1901", region: "台灣",country: "清水斷崖",url: "https://drive.google.com/file/d/1DgLM2KZuNNlWEqAl7aFYTMna7Rzupzfj/view?usp=drive_link" , memo1:""},
 {id: 1, year: "2024", region: "中國",country: "上海",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"(外灘)"},
 {id: 2, year: "2024", region: "中國",country: "上海",url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view" , memo1:""},
 {id: 3, year: "2024", region: "波羅的海三國",country: "立陶宛",url: "https://drive.google.com/file/d/1_x03q-TwGOZPaEqmhMkzj_g-4k_IpbSE/view" , memo1:"(維爾紐斯主教座堂)"},
